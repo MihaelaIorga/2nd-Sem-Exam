d35d0e035967d391a09f07e2b172aa14a8e134f2
mHampi <michaelahampachelova@gmail.com>
2017-12-06 23:35:00 +0100

    fixed responsivness for home page

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function showMusics(data) {
     data.forEach(function (theMusic) {
        console.log(theMusic);
         let clone = template.cloneNode(true);
-        let title = clone.querySelector("h1");
+        let title = clone.querySelector("h2");
         let date = clone.querySelector(".date span");
         let img = clone.querySelector("img");
         let audio = clone.querySelector("audio");
@@ -48,7 +48,7 @@ function showEvents(mydata) {
     data.forEach(function (theEvent) {
        console.log(theEvent);
         let clone = template.cloneNode(true);
-        let title = clone.querySelector("h1");
+        let title = clone.querySelector("h2");
         let date = clone.querySelector(".date span");
         let place = clone.querySelector(".place span");
         let button = clone.querySelector("button");
